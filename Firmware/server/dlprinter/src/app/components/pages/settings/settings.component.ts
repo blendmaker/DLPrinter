a71adf4bc81ee5a972f13bb780c27597b5f2e3aa
Klaus Daume <kdaume@Klauss-MacBook-Pro.local>
2018-12-06 20:09:58 +0100

    backup

--- Firmware/server/dlprinter/src/app/components/pages/settings/settings.component.ts
+++ Firmware/server/dlprinter/src/app/components/pages/settings/settings.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { from, of } from 'rxjs';
+import { WebSocketService } from 'src/app/services/web-socket.service';
+import { MessageInterface } from '../../../../../../../src/IpcWsMessages/MessageInterface';
+import { SettingsData } from '../../../../../../../src/Settings';
 
 @Component({
   selector: 'app-settings',
@@ -9,20 +13,45 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class SettingsComponent implements OnInit {
   form: FormGroup = new FormGroup({
     port: new FormControl('', [ Validators.required ]),
-    /* phys_width: new FormControl([ Validators.required ]),
-    phys_height: new FormControl([ Validators.required ]),
-    endstop_idle_state: new FormControl([ Validators.required ]),
-    endstop_triggered_state: new FormControl([ Validators.required ]),
-    z_max: new FormControl([ Validators.required ]),
-    z_min_speed: new FormControl([ Validators.required ]),
-    z_max_speed: new FormControl([ Validators.required ]),
-    z_acceleration: new FormControl([ Validators.required ]),
-    z_steps: new FormControl([ Validators.required ]), */
+    phys_width: new FormControl('', [ Validators.required ]),
+    phys_height: new FormControl('', [ Validators.required ]),
+    endstop_idle_state: new FormControl('', [ Validators.required ]),
+    endstop_triggered_state: new FormControl('', [ Validators.required ]),
+    max_layer_height: new FormControl('', [ Validators.required ]),
+    min_layer_height: new FormControl('', [ Validators.required ]),
+    max_burn_time: new FormControl('', [ Validators.required ]),
+    min_burn_time: new FormControl('', [ Validators.required ]),
+    z_max: new FormControl('', [ Validators.required ]),
+    z_min_speed: new FormControl('', [ Validators.required ]),
+    z_max_speed: new FormControl('', [ Validators.required ]),
+    z_acceleration: new FormControl('', [ Validators.required ]),
+    z_steps: new FormControl('', [ Validators.required ]),
     security: new FormControl(true),
   });
-  constructor() { }
+  settings: SettingsData;
+  constructor(private webSocketService: WebSocketService) { }
 
   ngOnInit() {
+    this.webSocketService.subscribe((msg: MessageInterface) => {
+      switch (msg.cmd) {
+        case 'get-settings' :
+          this.settingsToControls();
+      }
+    });
   }
 
+  protected sendSettings() {
+    this.controlsToSettings();
+  }
+
+  private controlsToSettings() {
+    of(this.form.controls).subscribe((ctrl) => {
+      console.log(ctrl);
+    });
+    console.log(this.form.controls);
+  }
+
+  private settingsToControls() {
+
+  }
 }
--- Firmware/src/settings.ts
+++ Firmware/src/settings.ts
@@ -54,6 +54,12 @@ export interface SettingsData {
     phys_height: number;
     endstop_idle_state: string|number;
     endstop_triggered_state: string|number;
+    max_layer_height: number,
+    min_layer_height: number,
+    max_burn_time: number,
+    min_burn_time: number,
+    slicers : [
+        { name : string, path : string, }],
     z_max: number;
     z_min_speed: number;
     z_max_speed: number;
