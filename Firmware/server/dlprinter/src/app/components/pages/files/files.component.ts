97977d238c9f37bfe384f73147f7ec37a711c633
Klaus Daume <kdaume@ucs.at>
2019-08-29 12:17:46 +0200

    npm audit fix, removed vulnerable dependencies

--- Firmware/server/dlprinter/src/app/components/pages/files/files.component.ts
+++ Firmware/server/dlprinter/src/app/components/pages/files/files.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { WebSocketService } from 'src/app/services/web-socket.service';
 import { SessionStorageService } from 'src/app/services/session-storage.service';
-import { Subscription, BehaviorSubject } from 'rxjs';
-import { filter, first, map, debounce, debounceTime } from 'rxjs/operators';
+import { BehaviorSubject } from 'rxjs';
+import { filter, first, map } from 'rxjs/operators';
 import { FileMeta } from '../../../../../../../src/Interfaces/FileMeta';
 import { FormGroup, FormControl } from '@angular/forms';
+import { untilDestroyed } from 'ngx-take-until-destroy';
 
 @Component({
   selector: 'app-files',
@@ -12,7 +13,8 @@ import { FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./files.component.scss']
 })
 export class FilesComponent implements OnInit, OnDestroy {
-  private subscription: Subscription[] = [];
+  // TODO: clear subscription arrays
+  // private subscription: Subscription[] = [];
   private remoteFiles: FileMeta[] = [];
   public files: FileMeta[] = [];
   public optionsForm = new FormGroup({
@@ -34,52 +36,47 @@ export class FilesComponent implements OnInit, OnDestroy {
       tableDisplay: this.sessionStorageService.filesDisplayOptions.value.tableDisplay,
     });
 
-    this.subscription.push(
-      this.ws.pipe(
-        filter( msg => msg.cmd === 'get-files'),
-        map( msg => msg.data.filter( file => {
-          if ( file.type === 'svg' &&
-            !this.sessionStorageService.filesDisplayOptions.value.displaySvg) {
-            return false;
-          } else if ( file.type === 'stl' &&
-            !this.sessionStorageService.filesDisplayOptions.value.displayStl) {
-            return false;
-          } else if ( file.type === 'gcode' &&
-            !this.sessionStorageService.filesDisplayOptions.value.displayGCodes) {
-            return false;
-          } else if ( file.type === 'other' &&
-            !this.sessionStorageService.filesDisplayOptions.value.displayOthers) {
-            return false;
-          }
-          return true;
-        })),
-      ).subscribe( files => { this.remoteFiles = files; this.filterDisplay.next(''); } )
-    );
-
-    this.subscription.push(
-      this.optionsForm.valueChanges.subscribe(
-        formData => {
-          this.sessionStorageService.filesDisplayOptions.next(formData);
-          this.ws.send({ cmd : 'get-files' });
+    this.ws.pipe(
+      untilDestroyed(this),
+      filter( msg => msg.cmd === 'get-files'),
+      map( msg => msg.data.filter( file => {
+        if ( file.type === 'svg' &&
+          !this.sessionStorageService.filesDisplayOptions.value.displaySvg) {
+          return false;
+        } else if ( file.type === 'stl' &&
+          !this.sessionStorageService.filesDisplayOptions.value.displayStl) {
+          return false;
+        } else if ( file.type === 'gcode' &&
+          !this.sessionStorageService.filesDisplayOptions.value.displayGCodes) {
+          return false;
+        } else if ( file.type === 'other' &&
+          !this.sessionStorageService.filesDisplayOptions.value.displayOthers) {
+          return false;
         }
-      )
+        return true;
+      })),
+    ).subscribe( files => { this.remoteFiles = files; this.filterDisplay.next(''); } );
+
+    this.optionsForm.valueChanges.pipe(untilDestroyed(this)).subscribe(
+      formData => {
+        this.sessionStorageService.filesDisplayOptions.next(formData);
+        this.ws.send({ cmd : 'get-files' });
+      }
     );
 
-    this.subscription.push(
-      this.filterDisplay.subscribe( term =>
-        this.files = this.remoteFiles.filter(file =>
-          (!term) ||
-          file.name.indexOf(term) !== -1 ||
-          (file.description && file.description.indexOf(term) !== -1))
-      )
+    this.filterDisplay.pipe(untilDestroyed(this)).subscribe( term =>
+      this.files = this.remoteFiles.filter(file =>
+        (!term) ||
+        file.name.indexOf(term) !== -1 ||
+        (file.description && file.description.indexOf(term) !== -1))
     );
 
     this.ws.connected.pipe( filter(connected => connected), first() ).subscribe( () => this.ws.send({ cmd : 'get-files' }) );
   }
 
   ngOnDestroy(): void {
-    this.subscription.forEach( subscription => subscription.unsubscribe() );
-    this.subscription = [];
+    // this.subscription.forEach( subscription => subscription.unsubscribe() );
+    // this.subscription = [];
   }
 
   public processFile(file: FileMeta) {
